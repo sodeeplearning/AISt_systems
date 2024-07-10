d7669c62337b5ad96e9045bc765159e51dd50faa
SFDL Vitally <147982026+sodeeplearning@users.noreply.github.com>
2024-07-10 21:22:37 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,7 @@
     So far, we have these modules:
         1) face - module for face recognition or face unlocking systems.
         2) utils - module with some stuff for systems.
+        3) watching - module for surveillance
+
+    You can look at these libraries in folders with corresponding names
 """
